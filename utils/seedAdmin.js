d6b5cb3b7c322e22c1765880fb622c321a050503
shahzahidnewaz <zahidnewazx123@gmail.com>
2026-08-21 00:19:05 +0600

    Second Commit

--- utils/seedAdmin.js
+++ utils/seedAdmin.js
@@ -1,20 +1,18 @@
 const { initDb } = require("../config/db");
 const { ask, closePrompt } = require("./prompt");
-const userModel = require("../models/userModel");
-const { getPool } = require("../config/db");
+const { User } = require("../models/associations");
 
 async function run() {
   await initDb();
   const email = await ask("Email of the user to promote to admin: ");
-  const user = await userModel.findByEmail(email);
+  const user = await User.findOne({ where: { email } });
   if (!user) {
     console.log("No user found with that email. Register them in the app first.");
   } else {
-    await getPool().query(`UPDATE users SET role = 'admin' WHERE id = ?`, [user.id]);
+    await user.update({ role: "admin" });
     console.log(`${user.firstname} ${user.lastname} is now an admin.`);
   }
   closePrompt();
   process.exit(0);
 }
-
 run();
